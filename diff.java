d498770cfeb765f42f2d50000b100d0ea8a0526b
Nayan <nayan92001@gmail.com>
2023-01-24 20:02:23 +0530

    Added message to diff.java

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diff {
     public static void main(String[] args) {
         int a = 20;
         int b = 10;
-        System.out.println(b-a);
+        System.out.println("Difference is " + (b-a));
     }
 }
